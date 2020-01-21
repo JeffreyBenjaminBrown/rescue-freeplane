8bb7172ecfac7f085433f9f3823511e3876121e5
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-01-20 19:22:01 -0500

    finish lesson 2

--- src/L2.hs
+++ src/L2.hs
@@ -3,18 +3,39 @@ module L2 where
 import Text.XML.HXT.Core
 
 
-play arg = do
-  results <- runX (processor arg)
-  print results
+r :: FilePath -> IOStateArrow s b XmlTree
+r filename = readDocument [withValidate no] filename
 
-processor :: FilePath -> IOSArrow XmlTree String
-processor filename =
-    readDocument [withValidate no] filename >>>
+-- runX $ r "data/a.xml" >>> title
+title :: IOSArrow XmlTree String
+title =
     getChildren >>>
-    isElem >>> hasName "html" >>>
+    isElem >>> -- upstream of this we could receive a processing instruction, a comment, a segment of whitespace text, or an element. downstream, only elements.
+    hasName "html" >>>
     getChildren >>>
     isElem >>> hasName "head" >>>
     getChildren >>>
     isElem >>> hasName "title" >>>
     getChildren >>>
     getText
+
+-- runX $ r "data/a.xml" >>> elemPlus
+elemPlus :: IOSArrow XmlTree XmlTree
+elemPlus = getChildren
+
+-- runX $ r "data/a.xml" >>> elem
+elem :: IOSArrow XmlTree XmlTree
+elem =
+    getChildren >>>
+    isElem
+
+-- runX $ r "data/a.xml" >>> hasTitle
+hasTitle :: IOSArrow XmlTree XmlTree
+hasTitle =
+    getChildren >>>
+    isElem >>>
+    hasName "html" >>>
+    getChildren >>>
+    isElem >>> hasName "head" >>>
+    getChildren >>>
+    isElem >>> hasName "title"
